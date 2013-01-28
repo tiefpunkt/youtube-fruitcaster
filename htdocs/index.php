7bb32cfeb1b878eac729e5a190cff249fa342576
Felix Arndt <mail@felixarndt.de>
2013-01-28 23:18:20 +0100

    add row highlighting

--- htdocs/index.php
+++ htdocs/index.php
@@ -43,7 +43,15 @@ foreach ($feed->get_items() as $feed_item){
 	<link href="css/bootstrap-responsive.css" rel="stylesheet">
 	<link href="css/custom.css" rel="stylesheet">
 	<meta charset="utf-8">
+	<script src="js/jquery.min.js"></script>
 	<script type="text/javascript">
+		$(document).ready(function() {
+                $("#" + window.location.hash.replace("#", "")).addClass("info");
+        });
+        window.onhashchange = function(){
+                $("tr").removeClass("info");
+                $("#" + window.location.hash.replace("#", "")).addClass("info");
+        };
     	/* <![CDATA[ */
     	(function() {
     	        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
